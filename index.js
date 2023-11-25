567b513bb385a95cb1df6649696f2dce24396bcf
Shumin <gsmzz.work@gmail.com>
2023-11-25 23:35:51 +0800

    commit2-search updated

--- index.js
+++ index.js
@@ -2,27 +2,57 @@ function recommend() {
     
     let searchTerm = document.querySelector('.form-control-lg').value.toLowerCase();
 
-    //let product = [
-        //{ name: "John Doe", details: "Product Director" },
-        //{ name: "Jane Smith", details: "Senior Product Manager" },
-    //];
 
-    if (searchTerm === "") {
-        displayResults("No mentors founds");
-        return;
-    };
+    function mentor (name, title, email) {
+        this.name = name;
+        this.title = title;
+        this.email = email;
+    }
+    let mentors = [ 
+    new mentor("John Doe", "Product Director", "john.doe@gov.sg"),
+    new mentor("Jane Smith", "Senior Product Manager", "jane.smith@gov.sg"),
+    new mentor("Jack Stone", "Data Scientist", "jack.stoneh@gov.sg"),
+    new mentor("Harry Daly", "Software Engineer", "harry.daly@gov.sg"),
+    new mentor("Joe Berg" , "Cybersecurity Engineer", "joe.berg@gov.sg")
+    ];
+
+    let filteredMentors = mentors.filter(mentor =>
+        Object.values(mentor).some(value =>
+            typeof value === "string" && value.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+    );
+
+    let resultsContainer = document.querySelector('.results'); 
+
+    resultsContainer.innerHTML = " ";
+ 
+            
+    if (filteredMentors.length > 0) {
+        // If mentors are found, display them
+
+        let headerElement = document.createElement("h2");
+        headerElement.textContent = (`Mentor results for "${searchTerm}"`);
+        resultsContainer.appendChild(headerElement);
+
+       
+        filteredMentors.forEach(mentor => {
+            let mentorInfo = document.createElement("p");
+            mentorInfo.textContent = `
+                Name: ${mentor.name}
+                Title: ${mentor.title}
+                Email: ${mentor.email}
+            `;
+            resultsContainer.appendChild(mentorInfo);
+                
+            resultsContainer.appendChild(document.createElement("hr"));
+            });
 
 
-    function displayResults(results) {
-        var resultsContainer = document.querySelector('.search-results'); 
-        
-        resultsContainer.innerHTML = '';
-
-        var resultsElement = document.createElement('p');
-        resultsElement.textContent = results;
-
-    // Step 8: Append the message element to the result container
-    resultsContainer.appendChild(resultsElement);
+    } else {
+        // If no mentors are found
+        let resultsElement = document.createElement("p");
+        resultsElement.textContent = "No mentors found";
+        resultsContainer.appendChild(resultsElement);
+    }
 
 }
-};
